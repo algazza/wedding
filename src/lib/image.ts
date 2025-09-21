184e616c51eadceb9bf8308fc3e72eb2ed8011b2
algazza <fadhilaswadira72564@gmail.com>
2025-09-21 07:24:19 +0700

    📝 feat: add image ts to reusable image import

---
+++ src/lib/image.ts
@@ -0,0 +1,37 @@
+import logo_wedding from "@/assets/Logo.png"
+import leaf from "@/assets/leaf.png"
+import flower from "@/assets/flower.png"
+import poem from "@/assets/Poem.png"
+import divider from "@/assets/Flower Divider.png"
+import holdingHand from "@/assets/hand.png"
+import Luri from "@/assets/Luri.png"
+import borederTitle from "@/assets/border Title.png"
+import Reza from "@/assets/Reza.png"
+import Prewed1 from "@/assets/prewed/1stprewed.jpg"
+import Prewed2 from "@/assets/prewed/2ndprewed.jpg"
+import Prewed3 from "@/assets/prewed/3rdprewed.jpg"
+import Prewed4 from "@/assets/prewed/4thprewed.jpg"
+import Prewed5 from "@/assets/prewed/5thprewed.jpg"
+import Prewed6 from "@/assets/prewed/6thprewed.jpg"
+import Prewed7 from "@/assets/prewed/7thprewed.jpg"
+import Prewed8 from "@/assets/prewed/closingprewed.jpg"
+
+export {
+    logo_wedding,
+    leaf,
+    flower,
+    poem,
+    divider,
+    holdingHand,
+    Luri,
+    borederTitle,
+    Reza,
+    Prewed1,
+    Prewed2,
+    Prewed3,
+    Prewed4,
+    Prewed5,
+    Prewed6,
+    Prewed7,
+    Prewed8,
+}
