4526ec42dd44358509870e903322de763b34a474
algazza <fadhilaswadira72564@gmail.com>
2025-09-25 10:51:03 +0700

    💄 ui: add ui and other

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -4,9 +4,11 @@ import flower from "@/assets/flower.png"
 import poem from "@/assets/Poem.png"
 import divider from "@/assets/Flower Divider.png"
 import holdingHand from "@/assets/hand.png"
+import bridengroom from "@/assets/Bride Groom.png"
 import Luri from "@/assets/Luri.png"
 import borederTitle from "@/assets/border Title.png"
 import Reza from "@/assets/Reza.png"
+import flower2 from "@/assets/flower 2.png"
 import Prewed1 from "@/assets/prewed/1stprewed.jpg"
 import Prewed2 from "@/assets/prewed/2ndprewed.jpg"
 import Prewed3 from "@/assets/prewed/3rdprewed.jpg"
@@ -14,7 +16,7 @@ import Prewed4 from "@/assets/prewed/4thprewed.jpg"
 import Prewed5 from "@/assets/prewed/5thprewed.jpg"
 import Prewed6 from "@/assets/prewed/6thprewed.jpg"
 import Prewed7 from "@/assets/prewed/7thprewed.jpg"
-import Prewed8 from "@/assets/prewed/closingprewed.jpg"
+import PrewedClose from "@/assets/prewed/closingprewed.jpg"
 
 export {
     logo_wedding,
@@ -23,9 +25,11 @@ export {
     poem,
     divider,
     holdingHand,
+    bridengroom,
     Luri,
     borederTitle,
     Reza,
+    flower2,
     Prewed1,
     Prewed2,
     Prewed3,
@@ -33,5 +37,5 @@ export {
     Prewed5,
     Prewed6,
     Prewed7,
-    Prewed8,
+    PrewedClose,
 }
