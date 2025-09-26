34925a0d22da8ce9c6fdf1229629da17fe747993
algazza <fadhilaswadira72564@gmail.com>
2025-09-26 23:06:48 +0700

    💄 ui: fix typos

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ const data = useLocalStorage<FormSchema>("form-data", {
   wish: "",
 });
 
-const isSuccess = useLocalStorage("isSuccess", true);
+const isSuccess = useLocalStorage<boolean>("isSuccess", true);
 
 const audioRef = ref<HTMLAudioElement | null>(null);
 const isMuted = ref(false);
@@ -316,7 +316,7 @@ const toggleAudio = () => {
           <p class="text-xl">
             {{
               isSuccess
-                ? "THANK YOU FOR YOUR RESPONSEfe"
+                ? "THANK YOU FOR YOUR RESPONSE"
                 : "PLEASE CONFIRM YOUR ATTENDANCE"
             }}
           </p>
